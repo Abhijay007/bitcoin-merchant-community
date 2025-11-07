ff70034daf10ba1c00afe9b54dc5faedfa194798
Abhijay007 <Abhijay007j@gmail.com>
2025-11-07 13:54:17 +0530

    refactor: add header of the website
    Signed-off-by: Abhijay007 <Abhijay007j@gmail.com>

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Box, Flex, Image, Text, Link as ChakraLink } from '@chakra-ui/react';
+
+const Header = () => {
+    return (
+        <Box bg="white" borderBottom="2px solid #000">
+            <Flex
+                as="nav"
+                align="center"
+                justify="space-between"
+                wrap="nowrap" 
+                padding={{ base: "0.5rem 1rem", md: "1.5rem 2rem", lg: "1.5rem 3rem" }}
+                color="gray.800"
+                maxW="1500px"
+                mx="auto"
+            >
+                {/* Logo Section */}
+                <Flex align="center">
+                    <Image 
+                        src="/assets/HeaderImages/logo.svg" 
+                        alt="Bitcoin Merchant Community Logo" 
+                        h={{ base: "40px", md: "50px", lg: "60px" }} 
+                    />
+                </Flex>
+
+                {/* Right Side Link */}
+                <Flex align="center">
+                    <ChakraLink
+                        href="#"
+                        fontSize={{ base: "14px", md: "16px", lg: "18px" }}
+                        fontWeight="400"
+                        color="black"
+                        textDecoration="underline"
+                        _hover={{
+                            textDecoration: "none",
+                            opacity: 0.8
+                        }}
+                    >
+                        Help us spread the word. Download a kit.
+                    </ChakraLink>
+                </Flex>
+            </Flex>
+        </Box>
+    );
+};
+
+export default Header;
+
